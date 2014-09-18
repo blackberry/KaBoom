db6b2f182fdf30645aeb8612a26850d6f46cdce6
mbruce <mbruce@rim.net>
2014-09-18 06:23:33 -0400

    Removing lagSecGaugeName after each worker run

--- src/main/java/com/blackberry/kaboom/Worker.java
+++ src/main/java/com/blackberry/kaboom/Worker.java
@@ -374,6 +374,8 @@ public class Worker implements Runnable {
 
 			MetricRegistrySingleton.getInstance().getMetricsRegistry()
 					.remove(lagGaugeName);
+			MetricRegistrySingleton.getInstance().getMetricsRegistry()
+			.remove(lagSecGaugeName);
 			LOG.info("[{}] Worker stopped. (Read {} lines.  Next offset is {})",
 					partitionId, linesread, offset);
 		} finally {
