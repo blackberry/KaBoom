d9e7646cc96cc40cd5d9357d4cc38e58a54bfe59
Dave Ariens <dariens@blackberry.com>
2014-10-08 11:53:28 -0400

    Checks for existing of merge's ready flag

--- src/main/java/com/blackberry/kaboom/ReadyFlagWriter.java
+++ src/main/java/com/blackberry/kaboom/ReadyFlagWriter.java
@@ -33,10 +33,12 @@ public class ReadyFlagWriter extends NotifyingThread
 	private static final Object fsLock = new Object();
 	private FileSystem fs;
 	private Configuration hConf;
-	private static final String ZK_ROOT = "/kaboom"; // TODO: This is currently a duplicate hard coded variable... Also exists in Worker.java
-	public static final String READY_FLAG = "_KAFKA_READY";
-	public static final String FLAG_DIR = "/incoming";
-	public static final String LOG_TAG = "[ready flag writer]";
+	
+	private static final String ZK_ROOT = "/kaboom"; 
+	public static final String MERGE_READY_FLAG = "_READY";
+	public static final String KAFKA_READY_FLAG = "_KAFKA_READY";
+	public static final String INCOMING_DIR = "/incoming";
+	public static final String LOG_TAG = "[ready flag writer] ";
 
 	public ReadyFlagWriter(String kafkaZkConnectionString,
 			String kafkaSeedBrokers, CuratorFramework curator,
@@ -52,25 +54,65 @@ public class ReadyFlagWriter extends NotifyingThread
 	}
 
 	/*
-	 * Takes an HDFS (or template) Looks for the last occurrence of a directory
-	 * Truncates after the last occurrence of the directory
+	 * Takes an HDFS path (or template) 
+	 * Returns the last occurrence of a sub directory
 	 */
 
-	public static String flagRootFromHdfsPath(String hdfsPath) 
+	public static String subDirectoryFromPath(String hdfsPath, String dir)
 	{
-		String flagRoot = new String();
-		int lastCharPos = hdfsPath.lastIndexOf(FLAG_DIR);	
+		String result = new String();
+		int lastCharPos = hdfsPath.lastIndexOf(dir);	
 		// Check to see if last occurrence of FLAG_DIR is the end of the string
-		if (hdfsPath.length() == lastCharPos + FLAG_DIR.length()) 
-		{
-			flagRoot = hdfsPath;
-		} else 
-		{
-			flagRoot = hdfsPath.substring(0, lastCharPos + FLAG_DIR.length());
+		if (hdfsPath.length() == lastCharPos + dir.length()) {
+			result = hdfsPath;
+		} else {
+			result = hdfsPath.substring(0, lastCharPos + dir.length());
 		}
-		return flagRoot;
+		return result;
 	}
 
+	/*
+	 * Takes an HDFS path (or template) and a sub directory
+	 * Returns the parent directory for the last occurrence of sub directory
+	 */
+
+	public static String parentFromPath(String hdfsPath, String dir) 
+	{
+		String result = new String();
+		int lastCharPos = hdfsPath.lastIndexOf(dir);	
+		result = hdfsPath.substring(0, lastCharPos);
+		return result;
+	}
+
+	/*
+	 * For a given topic and list of partitions
+	 * Returns the oldest timestamp stored in ZK for partition offset 
+	 */
+	
+	public long oldestPartitionOffsetForTopic(String topicName, List<PartitionMetadata> partitions) throws Exception
+	{
+		long oldestTimestamp = -1;
+		
+		for (PartitionMetadata partition : partitions) 
+		{
+			String zk_offset_path = ZK_ROOT + "/topics/" + topicName + "/" + partition.partitionId() + "/offset_timestamp";
+			Stat stat = this.curator.checkExists().forPath(zk_offset_path);
+			
+			if (stat != null) {
+				Long thisTimestamp = Converter.longFromBytes(curator.getData().forPath(zk_offset_path), 0);
+				LOG.debug(LOG_TAG + "found topic={} partition={} offset timestamp={}",topicName, partition.partitionId(), thisTimestamp);
+				if (thisTimestamp < oldestTimestamp || oldestTimestamp == -1) {
+					oldestTimestamp = thisTimestamp;
+				}
+				stat = null;
+			} else {
+				LOG.error(LOG_TAG + "cannot get stat for path {}", zk_offset_path);
+			}					
+		}
+		
+		return oldestTimestamp;		
+	}
+	
 	/*
 	 * Do we need to write the _KAFKA_READY flag for the last hour?
 	 * 
@@ -84,9 +126,9 @@ public class ReadyFlagWriter extends NotifyingThread
 	 * 
 	 * Note: This isn't intelligent enough to look earlier than the previous hour.
 	 * If this method isn't called at least once per hour then there will be flags
-	 * that will never bet set.
+	 * that will never become set.
 	 */
-	
+
 	@Override
 	public void doRun() throws Exception 
 	{
@@ -106,29 +148,30 @@ public class ReadyFlagWriter extends NotifyingThread
 			
 			String topicName = entry.getKey();
 			
-			try 
-			{
-				LOG.debug(LOG_TAG + " Checking {} partition(s) in topic={} for offset timestamps...", entry.getValue().size(), topicName);
+			try {
+				
+				LOG.debug(LOG_TAG + "Checking {} partition(s) in topic={} for offset timestamps...", entry.getValue().size(), topicName);
 
 				String hdfsTemplate = topicFileLocation.get(topicName);
 
-				if (hdfsTemplate == null) 
-				{
-					LOG.error(LOG_TAG + " HDFS path property for topic={} is not defined in configuraiton, skipping topic", topicName);
+				if (hdfsTemplate == null) {
+					LOG.error(LOG_TAG + "HDFS path property for topic={} is not defined in configuraiton, skipping topic", topicName);
 					continue;
 				}
 
-				String hdfsFlagRoot;
+				String hdfsPath = Converter.timestampTemplateBuilder(prevHourStartTimestmap, hdfsTemplate);
 				
-				hdfsFlagRoot = flagRootFromHdfsPath(hdfsTemplate);				
-				hdfsFlagRoot = Converter.timestampTemplateBuilder(prevHourStartTimestmap, hdfsFlagRoot);
+				final Path topicRoot = new Path(parentFromPath(hdfsPath, INCOMING_DIR));
+				final Path mergeReadyFlag = new Path(topicRoot + "/" + MERGE_READY_FLAG);
+				final Path kafkaReadyFlag = new Path(topicRoot + "/" + KAFKA_READY_FLAG);				
+				final Path incomingDirectory = new Path(topicRoot + "/"  + INCOMING_DIR);
 				
-				final Path hdfsFlagRootPath = new Path(hdfsFlagRoot);
-				final Path hdfsFlagFilePath = new Path(hdfsFlagRoot + "/" + READY_FLAG);
+				LOG.debug(LOG_TAG + "HDFS path for topic root is: {}", topicRoot.toString());
+				LOG.debug(LOG_TAG + "HDFS path for merge ready flag is: {}", mergeReadyFlag.toString());				
+				LOG.debug(LOG_TAG + "HDFS path for kafka ready flag is: {}", kafkaReadyFlag.toString());
+				LOG.debug(LOG_TAG + "HDFS path for incoming directory is: {}", incomingDirectory.toString());
+				LOG.debug(LOG_TAG + "opening {}", topicRoot.toString());
 				
-				LOG.debug(LOG_TAG + " HDFS root for the ready flag is: {}", hdfsFlagRootPath.toString());
-				LOG.debug(LOG_TAG + " HDFS path for the ready flag is: {}", hdfsFlagFilePath.toString());				
-
 				Authenticator.getInstance().runPrivileged(topicToProxyUser.get(topicName),
 						new PrivilegedExceptionAction<Void>() 
 						{
@@ -136,80 +179,53 @@ public class ReadyFlagWriter extends NotifyingThread
 							public Void run() throws Exception {
 								synchronized (fsLock) {
 									try {
-										fs = hdfsFlagRootPath.getFileSystem(hConf);
+										fs = topicRoot.getFileSystem(hConf);
 									} catch (IOException e) {
-										LOG.error(LOG_TAG + " Error getting File System for path {}, error: {}.", hdfsFlagRootPath.toString(), e);
+										LOG.error(LOG_TAG + "Error getting file system for path {}, error: {}.", topicRoot.toString(), e);
 									}
 								}
 								return null;
 							}
-						});
+						});				
 
-				LOG.debug(LOG_TAG + " opening {}", hdfsFlagFilePath.toString());
-
-				if (fs.exists(hdfsFlagFilePath)) 
-				{
-					LOG.debug(LOG_TAG + " flag {} already exists at {}", READY_FLAG, hdfsFlagFilePath.toString());
+				if (fs.exists(incomingDirectory)) {
+					LOG.debug(LOG_TAG + "skipping {} since incoming directory {} doesn't exist (no data)", topicName, incomingDirectory.toString());
 					continue;
 				}
-				else
-				{
-					LOG.debug(LOG_TAG + " flag {} doesn't exist yet at {}", READY_FLAG, hdfsFlagFilePath.toString());
-				}
 				
-				if (!fs.exists(hdfsFlagRootPath))
-				{
-					LOG.debug(LOG_TAG + " flag_root {} doesn't exist (no data)", hdfsFlagRootPath.toString());
+				if (fs.exists(mergeReadyFlag))  {
+					LOG.debug(LOG_TAG + "skipping {} since merge's ready flag {} already exists", topicName, mergeReadyFlag.toString());
 					continue;
 				}
-				else
-				{
-					LOG.debug(LOG_TAG + " flag_root {} does in fact exist (there is data)", hdfsFlagRootPath.toString());
+				
+				if (fs.exists(kafkaReadyFlag))  {
+					LOG.debug(LOG_TAG + "skipping {} since kafka's ready flag {} already exists", topicName, kafkaReadyFlag.toString());
+					continue;
 				}
 				
-				long oldestTimestamp = -1;
+				LOG.debug(LOG_TAG + "topic {} might be candidate for kafka ready flag (incoming data exists, no other flags exist)", topicName);
+				long oldestTimestamp = oldestPartitionOffsetForTopic(topicName, entry.getValue());
+				LOG.debug(LOG_TAG + "oldest timestamp for topic={} is {}", topicName, oldestTimestamp);
 
-				for (PartitionMetadata partition : entry.getValue()) 
-				{
-					String zk_offset_path = ZK_ROOT + "/topics/" + topicName + "/" + partition.partitionId() + "/offset_timestamp";
-					Stat stat = curator.checkExists().forPath(zk_offset_path);
-					
-					if (stat != null) {
-						Long thisTimestamp = Converter.longFromBytes(curator.getData().forPath(zk_offset_path), 0);
-						LOG.debug(LOG_TAG + " found topic={} partition={} offset timestamp={}",topicName, partition.partitionId(), thisTimestamp);
-						if (thisTimestamp < oldestTimestamp || oldestTimestamp == -1) {
-							oldestTimestamp = thisTimestamp;
-						}
-						stat = null;
-					} else {
-						LOG.error(LOG_TAG + " cannot get stat for path {}", zk_offset_path);
-					}					
-				}
-
-				LOG.debug(LOG_TAG + " oldest timestamp for topic={} is {}", topicName, oldestTimestamp);
-
-				if (oldestTimestamp > startOfHourTimestamp) 
-				{
-					LOG.debug(LOG_TAG + " oldest timestamp is within the current hour, flag write required");
-					LOG.debug(LOG_TAG + " need to write {} as proxy user {}", hdfsFlagFilePath.toString(), topicToProxyUser.get(topicName));
-
+				if (oldestTimestamp > startOfHourTimestamp) {
+					LOG.debug(LOG_TAG + "topic {} oldest timestamp is within the current hour, flag write required", topicName);
+					LOG.debug(LOG_TAG + "topic {} need to write {} as proxy user {}", topicName, kafkaReadyFlag.toString(), topicToProxyUser.get(topicName));
 					synchronized (fsLock) {
 						try {
-							fs.create(hdfsFlagFilePath).close();
+							fs.create(kafkaReadyFlag).close();
 						} catch (IOException e) {
 							LOG.error("Error getting File System: {}", e.toString());
 						}
 					}
-
-					LOG.debug(LOG_TAG + " flag {} written as {}", hdfsFlagFilePath.toString(), topicToProxyUser.get(topicName));
+					LOG.debug(LOG_TAG + "topic {} flag {} written as {}", topicName, kafkaReadyFlag.toString(), topicToProxyUser.get(topicName));
 				}
+				
 			} 
-			catch (Exception e) 
-			{
-				LOG.error(LOG_TAG + " error occured processing a partition: {}", e.toString());
+			catch (Exception e) {
+				LOG.error(LOG_TAG + "topic {} error occured processing a partition: {}", topicName, e.toString());
 			}
 
-			LOG.debug(LOG_TAG + " finished {} topic(s) after {} seconds", topicsWithPartitions.size(), (cal.getTimeInMillis() - currentTimestamp) / 1000);
+			LOG.debug(LOG_TAG + "finished {} topic(s) after {} seconds", topicsWithPartitions.size(), (cal.getTimeInMillis() - currentTimestamp) / 1000);
 		}
 	}
 }
