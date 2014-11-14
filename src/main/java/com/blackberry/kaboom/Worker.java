4515d575d31c53dc065918646be802dbc37ee86a
Dave Ariens <dariens@blackberry.com>
2014-11-14 15:14:23 +0000

    adding copyright and license

--- src/main/java/com/blackberry/kaboom/Worker.java
+++ src/main/java/com/blackberry/kaboom/Worker.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 BlackBerry, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.blackberry.kaboom;
 
 import java.io.IOException;
@@ -636,7 +652,11 @@ public class Worker implements Runnable
 			MetricRegistrySingleton.getInstance().getMetricsRegistry().remove(msgWrittenGaugeName);
 			
 			LOG.info("[{}] Worker stopped. (Read {} lines.  Next offset is {})", partitionId, linesread, offset);
-		} 
+		}
+		catch (Exception e) 
+		{
+			LOG.error("[{}] An exception occured while setting up this worker thread giving up and returing => error : {} ", partitionId, e);
+		}
 		finally
 		{
 			synchronized (workersLock)
