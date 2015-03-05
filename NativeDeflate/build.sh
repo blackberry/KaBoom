4fa37f33d62d2964ba8ccb42bc3845ec766d9f2f
Dave Ariens <dariens@blackberry.com>
2015-03-05 20:33:59 +0000

    another test

--- NativeDeflate/build.sh
+++
@@ -1,3 +0,0 @@
-$JAVA_HOME/bin/javah -cp "target/*"  com.blackberry.bdp.kaboom.FastBoomWriter
-
-gcc  -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux/ -shared -o libNativeDeflate.so NativeDeflate.c -fPIC -lz
