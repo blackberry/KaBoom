2c30e03ea05e47159a5ab182940bda584d72e191
Dave Ariens <dariens@blackberry.com>
2015-09-01 16:03:01 +0000

    Formatting FastBoomWriter.java

--- src/main/java/com/blackberry/bdp/kaboom/FastBoomWriter.java
+++ src/main/java/com/blackberry/bdp/kaboom/FastBoomWriter.java
@@ -25,16 +25,16 @@ import org.apache.hadoop.hdfs.client.HdfsDataOutputStream.SyncFlag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FastBoomWriter
-{
+public class FastBoomWriter {
+
 	private static final Logger LOG = LoggerFactory.getLogger(FastBoomWriter.class);
 	private static final Charset UTF8 = Charset.forName("UTF8");
-	private long lastHdfsFlushTimestamp = System.currentTimeMillis();	
+	private long lastHdfsFlushTimestamp = System.currentTimeMillis();
 	private long numAvroBlocksWritten = 0l;
 	private long numHdfsFlushedAVroBlocks = 0l;
 	private RunningConfig runningConfig = null;
 	private final String partitionId;
-	private Long periodicHdfsFlushInterval = null;	
+	private Long periodicHdfsFlushInterval = null;
 	private final Timer hdfsFlushTimerTopic;
 	private final Timer hdfsFlushTimerTotal;
 	private final Timer hdfsFlushTimer;
@@ -43,10 +43,10 @@ public class FastBoomWriter
 	private boolean useNativeCompression = false;
 	private final short compressionLevel;
 	private final Histogram compressionRatioHistogramTopic;
-	private final Histogram compressionRatioHistogramTotal;	
+	private final Histogram compressionRatioHistogramTotal;
 	private int compressedSize;
 	private byte[] compressedBlockBytes = new byte[256 * 1024];
-	private final Deflater deflater;	
+	private final Deflater deflater;
 	private long avroBlockRecordCount = 0L;
 	private final byte[] avroBlockBytes = new byte[2 * 1024 * 1024];
 	private final ByteBuffer avroBlockBuffer = ByteBuffer.wrap(avroBlockBytes);
@@ -58,48 +58,20 @@ public class FastBoomWriter
 	private final ByteBuffer logBlockBuffer = ByteBuffer.wrap(logBlockBytes);
 	private long logLineCount;
 	private final byte[] logLinesBytes = new byte[logBlockBytes.length - 41];
-	private final ByteBuffer logLinesBuffer = ByteBuffer.wrap(logLinesBytes);	
+	private final ByteBuffer logLinesBuffer = ByteBuffer.wrap(logLinesBytes);
 	private final byte[] longBytes = new byte[10];
 	private final ByteBuffer longBuffer = ByteBuffer.wrap(longBytes);
-	
-	private static final byte[] MAGIC_NUMBER = new byte[]
-	{
-		'O', 'b', 'j', 1		 
+
+	private static final byte[] MAGIC_NUMBER = new byte[]{
+		'O', 'b', 'j', 1
 	};
-	
-	public void loadNativeDeflateLib()
-	{
+
+	public void loadNativeDeflateLib() {
 		System.loadLibrary("NativeDeflate");
 	}
 
 	public native byte[] compress(byte[] bytesIn, int position, int compressionLevel);
-	
-	/**
-	 * Boom Avro Schema:
-	 * 
-		{
-		  "type": "record",
-		  "name": "logBlock",
-		  "fields": [
-			 { "name": "second",      "type": "long" },
-			 { "name": "createTime",  "type": "long" },
-			 { "name": "blockNumber", "type": "long" },
-			 { "name": "logLines", "type": {
-				"type": "array",
-				  "items": {
-					 "type": "record",
-					 "name": "messageWithMillis",
-					 "fields": [ 
-						{ "name": "ms", "type": "long" },
-						{ "name": "eventId", "type": "int", "default": 0 },
-						{ "name": "message", "type": "string" }
-					 ]
-				  }
-			 }}
-		  ]
-		}
-	*/
-	
+
 	private static final String SCHEMA_STRING = "{\"type\":\"record\",\"name\":\"logBlock\","
 		 + "\"fields\":["
 		 + "{\"name\":\"second\",\"type\":\"long\"},"
@@ -109,107 +81,90 @@ public class FastBoomWriter
 		 + "{\"type\":\"array\",\"items\":{\"type\":\"record\",\"name\":\"messageWithMillis\",\"fields\":["
 		 + "{\"name\":\"ms\",\"type\":\"long\"},"
 		 + "{\"name\":\"message\",\"type\":\"string\"}]}}}]}";
-	
+
 	private static final byte[] SCHEMA_BYTES = SCHEMA_STRING.getBytes(UTF8);
 
 	private final byte[] syncMarker;
 
-	private final HdfsDataOutputStream  hdfsDataOut;
+	private final HdfsDataOutputStream hdfsDataOut;
 
-	public FastBoomWriter(HdfsDataOutputStream out,  String topic, int partition, StartupConfig startupConfig) throws IOException
-	{
-		this.hdfsDataOut = out;		
+	public FastBoomWriter(HdfsDataOutputStream out, String topic, int partition, StartupConfig startupConfig) throws IOException {
+		this.hdfsDataOut = out;
 		this.partitionId = topic + "-" + partition;
 		this.runningConfig = startupConfig.getRunningConfig();
 		this.compressionLevel = runningConfig.getCompressionLevel();
-		this.deflater = new Deflater(compressionLevel, true);				
+		this.deflater = new Deflater(compressionLevel, true);
 		this.hdfsFlushTimerTopic = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:topic:" + topic + ":hdfs flush timer");
 		this.hdfsFlushTimerTotal = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:total:hdfs flush timer");
 		this.compressionTimerTotal = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:total:compression timer");
 		this.compressionTimerTopic = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:topic:" + topic + ":compression timer");
 		this.compressionRatioHistogramTopic = MetricRegistrySingleton.getInstance().getMetricsRegistry().histogram("kaboom:topic:" + topic + ":compresssion ratio");
 		this.compressionRatioHistogramTotal = MetricRegistrySingleton.getInstance().getMetricsRegistry().histogram("kaboom:total:compression ratio");
-		this.hdfsFlushTimer = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:partitions:" + this.partitionId + ":flush timer");				
+		this.hdfsFlushTimer = MetricRegistrySingleton.getInstance().getMetricsRegistry().timer("kaboom:partitions:" + this.partitionId + ":flush timer");
 		this.periodicHdfsFlushInterval = runningConfig.getPeriodicHdfsFlushInterval();
 		this.useNativeCompression = runningConfig.getUseNativeCompression();
-		
+
 		Random rand = new Random();
 		syncMarker = new byte[16];
 		rand.nextBytes(syncMarker);
 
 		writeHeader();
 	}
-	
-	private Long msSinceLastHdfsFlush() 
-	{		
+
+	private Long msSinceLastHdfsFlush() {
 		return System.currentTimeMillis() - lastHdfsFlushTimestamp;
 	}
-	
-	public void periodicHdfsFlushPoll() throws IOException
-	{
-		if (getPeriodicHdfsFlushInterval() == null 
+
+	public void periodicHdfsFlushPoll() throws IOException {
+		if (getPeriodicHdfsFlushInterval() == null
 			 || getPeriodicHdfsFlushInterval() == 0
-			 || msSinceLastHdfsFlush() < getPeriodicHdfsFlushInterval())
-		{
+			 || msSinceLastHdfsFlush() < getPeriodicHdfsFlushInterval()) {
 			return;
-		}		
-				
+		}
+
 		final Timer.Context timerContextTopic = hdfsFlushTimerTopic.time();
 		final Timer.Context timerContext = hdfsFlushTimer.time();
 		final Timer.Context timerContextTotal = hdfsFlushTimerTotal.time();
-		
-		try
-		{
+
+		try {
 			Boolean logBlockBufferWritten = false;
 
-			if (logBlockBuffer.position() > 0)
-			{
+			if (logBlockBuffer.position() > 0) {
 				LOG.trace("Log block write forced during periodic HDFS flush since buffer position is {}", logBlockBuffer.position());
-				writeLogBlock();				
+				writeLogBlock();
 				logBlockBufferWritten = true;
-			}
-			else
-			{
-				LOG.trace("Skipping forced log block write in periodic HDFS flush since log block buffer position is {}", logBlockBuffer.position());					 
+			} else {
+				LOG.trace("Skipping forced log block write in periodic HDFS flush since log block buffer position is {}", logBlockBuffer.position());
 			}
 
 			// Need to check time since last avro write again as writing the log block could call the avro block write
-
-			if (avroBlockBuffer.position() > 0)
-			{
+			if (avroBlockBuffer.position() > 0) {
 				LOG.trace("Avro block write forced during periodic HDFS flush since buffer position is {}", avroBlockBuffer.position());
 				writeAvroBlock();
-			}
-			else
-			{
-				if (logBlockBufferWritten == true)
-				{
+			} else {
+				if (logBlockBufferWritten == true) {
 					LOG.trace("A log block write was forced and likely incured a call to write the avro block because the avro block buffer position is now {}", avroBlockBuffer.position());
 				}
 
-				LOG.trace("Skipping forced avro block write since avro block buffer position is {}", avroBlockBuffer.position());					 
+				LOG.trace("Skipping forced avro block write since avro block buffer position is {}", avroBlockBuffer.position());
 			}
 
-			if (numAvroBlocksWritten == 0 || numHdfsFlushedAVroBlocks == numAvroBlocksWritten)
-			{
+			if (numAvroBlocksWritten == 0 || numHdfsFlushedAVroBlocks == numAvroBlocksWritten) {
 				LOG.trace("Skipping forced HDFS flush as there haven't been any new avro blocks written");
 				return;
-			}		
-			
+			}
+
 			hdfsDataOut.hsync(EnumSet.of(SyncFlag.UPDATE_LENGTH));
 			numHdfsFlushedAVroBlocks = numAvroBlocksWritten;
 			lastHdfsFlushTimestamp = System.currentTimeMillis();
-		}
-		finally
-		{
+		} finally {
 			timerContext.stop();
 			timerContextTopic.stop();
 			timerContextTotal.stop();
 		}
 	}
 
-	private void writeHeader() throws IOException
-	{
+	private void writeHeader() throws IOException {
 		hdfsDataOut.write(MAGIC_NUMBER);
 
 		// 2 entries in the metadata
@@ -231,51 +186,40 @@ public class FastBoomWriter
 		hdfsDataOut.write(syncMarker);
 	}
 
-	private void writeBytes(byte[] bytes) throws IOException
-	{
+	private void writeBytes(byte[] bytes) throws IOException {
 		encodeLong(bytes.length);
 		hdfsDataOut.write(longBytes, 0, longBuffer.position());
 		hdfsDataOut.write(bytes);
 	}
 
-	private void encodeLong(long n)
-	{
+	private void encodeLong(long n) {
 		longBuffer.clear();
 		n = (n << 1) ^ (n >> 63);
-		if ((n & ~0x7FL) != 0)
-		{
+		if ((n & ~0x7FL) != 0) {
 			longBuffer.put((byte) ((n | 0x80) & 0xFF));
 			n >>>= 7;
-			if (n > 0x7F)
-			{
+			if (n > 0x7F) {
 				longBuffer.put((byte) ((n | 0x80) & 0xFF));
 				n >>>= 7;
-				if (n > 0x7F)
-				{
+				if (n > 0x7F) {
 					longBuffer.put((byte) ((n | 0x80) & 0xFF));
 					n >>>= 7;
-					if (n > 0x7F)
-					{
+					if (n > 0x7F) {
 						longBuffer.put((byte) ((n | 0x80) & 0xFF));
 						n >>>= 7;
-						if (n > 0x7F)
-						{
+						if (n > 0x7F) {
 							longBuffer.put((byte) ((n | 0x80) & 0xFF));
 							n >>>= 7;
-							if (n > 0x7F)
-							{
+							if (n > 0x7F) {
 								longBuffer.put((byte) ((n | 0x80) & 0xFF));
 								n >>>= 7;
-								if (n > 0x7F)
-								{
+								if (n > 0x7F) {
 									longBuffer.put((byte) ((n | 0x80) & 0xFF));
 									n >>>= 7;
-									if (n > 0x7F)
-									{
+									if (n > 0x7F) {
 										longBuffer.put((byte) ((n | 0x80) & 0xFF));
 										n >>>= 7;
-										if (n > 0x7F)
-										{
+										if (n > 0x7F) {
 											longBuffer.put((byte) ((n | 0x80) & 0xFF));
 											n >>>= 7;
 										}
@@ -290,28 +234,20 @@ public class FastBoomWriter
 		longBuffer.put((byte) n);
 	}
 
-	public void writeLine(long timestamp, byte[] message, int offset, int length) throws IOException
-	{		
+	public void writeLine(long timestamp, byte[] message, int offset, int length) throws IOException {
 		ms = timestamp % 1000l;
 		second = timestamp / 1000l;
 
 		// If the buffer is too full to hold it, or the second has changed, then
 		// write out the block first.
 		if ((logBlockBuffer.position() > 0 && second != logBlockSecond)
-			 || logLinesBytes.length - logLinesBuffer.position() < 10 + 10 + length)
-		{
-			if (logBlockBuffer.position() > 0 && second != logBlockSecond)
-			{
+			 || logLinesBytes.length - logLinesBuffer.position() < 10 + 10 + length) {
+			if (logBlockBuffer.position() > 0 && second != logBlockSecond) {
 				LOG.debug("[{}] New Block ({} lines) (second changed from {} to {})", partitionId, logLineCount, logBlockSecond, second);
-			} 
-			else
-			{
-				if (logLinesBytes.length - logLinesBuffer.position() < 10 + 10 + length)
-				{
+			} else {
+				if (logLinesBytes.length - logLinesBuffer.position() < 10 + 10 + length) {
 					LOG.trace("[{}] New Block. ({} lines) (buffer full)", partitionId, logLineCount);
-				} 
-				else
-				{
+				} else {
 					LOG.trace("[{}] New Block. ({} lines)", partitionId, logLineCount);
 				}
 			}
@@ -319,8 +255,7 @@ public class FastBoomWriter
 			writeLogBlock();
 		}
 
-		if (logBlockBuffer.position() == 0)
-		{
+		if (logBlockBuffer.position() == 0) {
 			logBlockBuffer.clear();
 			logLinesBuffer.clear();
 			logLineCount = 0;
@@ -342,22 +277,18 @@ public class FastBoomWriter
 		/*
 		 * aryder: added try-catch back in to catch errors
 		 */
-		try
-		{
+		try {
 			LOG.debug("[{}] logBlockBuffer: CurPosition {}, InsertMessage-Offset {}, InsertMessage Length {}", partitionId, logBlockBuffer.position(), offset, length);
 
-			
 			encodeLong(ms);
 			logLinesBuffer.put(longBytes, 0, longBuffer.position());
 
 			encodeLong(length);
 			logLinesBuffer.put(longBytes, 0, longBuffer.position());
 			logLinesBuffer.put(message, offset, length);
-		} 
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			LOG.info("[{}] Exception! Buffer:{}, Length:{}", partitionId, logLinesBuffer, length, e);
-			
+
 			LOG.info("[{}] ???.  {} - {} < 10 + 10 + {}", partitionId, logBlockBytes.length,
 				 logBlockBuffer.position(), length);
 		}
@@ -365,29 +296,27 @@ public class FastBoomWriter
 		periodicHdfsFlushPoll();
 	}
 
-	private void writeLogBlock() throws IOException
-	{
+	private void writeLogBlock() throws IOException {
 		// We need room for the logBlockBuffer, the number of records in
 		// logLinesBuffer (up to 10) and the logLinesBuffer. If not, then we need to flush.
-		
+
 		if (avroBlockBytes.length - avroBlockBuffer.position() < logBlockBuffer
-			 .position() + 10 + logLinesBuffer.position())
-		{
+			 .position() + 10 + logLinesBuffer.position()) {
 			writeAvroBlock();
 		}
-		
+
 		LOG.debug("[{}] avroBlockBuffer adding logBlockBytes: CurPosition {}, insert length {}", partitionId, avroBlockBuffer.position(), logBlockBuffer.position());
 
 		avroBlockBuffer.put(logBlockBytes, 0, logBlockBuffer.position());
-		
+
 		encodeLong(logLineCount);
 		avroBlockBuffer.put(longBytes, 0, longBuffer.position());
-		
+
 		LOG.debug("[{}] avroBlockBuffer adding logLineBytes: CurPosition {}, insert length {}", partitionId, avroBlockBuffer.position(), logLinesBuffer.position());
-		
+
 		avroBlockBuffer.put(logLinesBytes, 0, logLinesBuffer.position());
-		
-		encodeLong(0L);		
+
+		encodeLong(0L);
 		avroBlockBuffer.put(longBytes, 0, longBuffer.position());
 
 		avroBlockRecordCount++;
@@ -396,132 +325,112 @@ public class FastBoomWriter
 		logLineCount = 0L;
 		logLinesBuffer.clear();
 	}
-	
-	private void writeAvroBlock() throws IOException
-	{
+
+	private void writeAvroBlock() throws IOException {
 		final Timer.Context timerTotal = compressionTimerTotal.time();
 		final Timer.Context timerTopic = compressionTimerTopic.time();
-		
-		try
-		{
+
+		try {
 			LOG.debug("[{}] Writing Avro Block ({} bytes)", partitionId, avroBlockBuffer.position());
 			encodeLong(avroBlockRecordCount);
 			hdfsDataOut.write(longBytes, 0, longBuffer.position());
-			
-			long start = System.currentTimeMillis();			
-				 
-			if (useNativeCompression)
-			{
+
+			long start = System.currentTimeMillis();
+
+			if (useNativeCompression) {
 				compressedBlockBytes = new byte[256 * 1024];
 				compressedBlockBytes = compress(avroBlockBytes, avroBlockBuffer.position(), compressionLevel);
-				compressedSize = compressedBlockBytes.length;	
-								
-				LOG.debug("[{}] Natively compressed {} bytes to {} bytes ({}% reduction), compression level {}", 
-					 partitionId, 
-					 avroBlockBuffer.position(), 
-					 compressedSize, 
+				compressedSize = compressedBlockBytes.length;
+
+				LOG.debug("[{}] Natively compressed {} bytes to {} bytes ({}% reduction), compression level {}",
+					 partitionId,
+					 avroBlockBuffer.position(),
+					 compressedSize,
 					 Math.round(100 - (100.0 * compressedSize / avroBlockBuffer.position())),
 					 compressionLevel);
-			}
-			else
-			{
-				while (true)
-				{
+			} else {
+				while (true) {
 					deflater.reset();
 					deflater.setInput(avroBlockBytes, 0, avroBlockBuffer.position());
 					deflater.finish();
 
 					compressedSize = deflater.deflate(compressedBlockBytes, 0, compressedBlockBytes.length);
 
-					if (compressedSize == compressedBlockBytes.length)
-					{
+					if (compressedSize == compressedBlockBytes.length) {
 						// it probably didn't actually compress all of it. Expand and retry
 						LOG.trace("[{}] Expanding compression buffer {} -> {}", partitionId, compressedBlockBytes.length, compressedBlockBytes.length * 2);
 						compressedBlockBytes = new byte[compressedBlockBytes.length * 2];
-					}
-					else
-					{						
+					} else {
 						break;
 					}
 				}
 			}
-			
-			LOG.debug("[{}] Compressed {} bytes to {} bytes ({}% reduction) in {} ms (native={}, compression level={})", 
+
+			LOG.debug("[{}] Compressed {} bytes to {} bytes ({}% reduction) in {} ms (native={}, compression level={})",
 				 partitionId,
-				 avroBlockBuffer.position(), 
-				 compressedSize, 
+				 avroBlockBuffer.position(),
+				 compressedSize,
 				 Math.round(100 - (100.0 * compressedSize / avroBlockBuffer.position())),
 				 System.currentTimeMillis() - start,
 				 useNativeCompression,
-				 compressionLevel);			
-			
+				 compressionLevel);
+
 			compressionRatioHistogramTopic.update(Math.round(100 - (100.0 * compressedSize / avroBlockBuffer.position())));
 			compressionRatioHistogramTotal.update(Math.round(100 - (100.0 * compressedSize / avroBlockBuffer.position())));
-			
+
 			encodeLong(compressedSize);
 
 			hdfsDataOut.write(longBytes, 0, longBuffer.position());
 			hdfsDataOut.write(compressedBlockBytes, 0, compressedSize);
-			hdfsDataOut.write(syncMarker);						
-		}
-		catch (Exception e)
-		{
-			LOG.error("[{}] error occured either compressing or writing the avro block: ", partitionId, e);			
-		}
-		finally
-		{
+			hdfsDataOut.write(syncMarker);
+		} catch (Exception e) {
+			LOG.error("[{}] error occured either compressing or writing the avro block: ", partitionId, e);
+		} finally {
 			timerTopic.stop();
 			timerTotal.stop();
-			
+
 			avroBlockBuffer.clear();
 			avroBlockRecordCount = 0L;
 			numAvroBlocksWritten++;
-		}		
+		}
 	}
 
-	public void close() throws IOException
-	{
-		if (logBlockBuffer.position() > 0)
-		{
+	public void close() throws IOException {
+		if (logBlockBuffer.position() > 0) {
 			writeLogBlock();
 		}
-		
-		if (avroBlockBuffer.position() > 0)
-		{
+
+		if (avroBlockBuffer.position() > 0) {
 			writeAvroBlock();
 		}
-		hdfsDataOut.close();		
+		hdfsDataOut.close();
 	}
 
 	/**
 	 * @return the periodicHdfsFlushInterval
 	 */
-	public Long getPeriodicHdfsFlushInterval()
-	{
+	public Long getPeriodicHdfsFlushInterval() {
 		return periodicHdfsFlushInterval;
 	}
 
 	/**
 	 * @param periodicHdfsFlushInterval the periodicHdfsFlushInterval to set
 	 */
-	public void setPeriodicHdfsFlushInterval(Long periodicHdfsFlushInterval)
-	{
+	public void setPeriodicHdfsFlushInterval(Long periodicHdfsFlushInterval) {
 		this.periodicHdfsFlushInterval = periodicHdfsFlushInterval;
 	}
 
 	/**
 	 * @return the useNativeCompression
 	 */
-	public boolean isUseNativeCompression()
-	{
+	public boolean isUseNativeCompression() {
 		return useNativeCompression;
 	}
 
 	/**
 	 * @param useNativeCompression the useNativeCompression to set
 	 */
-	public void setUseNativeCompression(boolean useNativeCompression)
-	{
+	public void setUseNativeCompression(boolean useNativeCompression) {
 		this.useNativeCompression = useNativeCompression;
 	}
 
